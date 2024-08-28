bca3d4bc37e4076133337171f5ba68c20dc38194
risi <Risi@users.noreply.github.com>
2024-08-28 21:57:09 +0400

    Reset Project✨

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Packages from "./components/Packages";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <main className="min-h-screen w-screen flex flex-col gap-16 items-center text-white text-xl pb-16">
-
-      {/* Hero */}
-      <Hero /> 
-      
-      {/* About (METRICS) */}
-      <About />
-
-      {/* Packages */}
-      <Packages />
-
-    </main>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
-export default App;
+export default App
