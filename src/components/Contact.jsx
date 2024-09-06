a7d2b324fbe2aedfee46b1bc723470c5c0063374
= <=>
2024-09-06 20:53:31 +0400

    added footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 function Contact () {
     return (
-        <section className="bg-black px-12 py-24 mt-16 w-screen flex flex-col items-center" id="contact">
+        <section className="bg-black px-12 pt-24 pb-4 mt-16 w-screen flex flex-col items-center" id="contact">
                 <h1 className="font-black font-title text-4xl lg:6xl uppercase text-center text-white">contact us</h1>
                 <p className="font-medium font-sub text-gray-400 text-xl mt-4"> drop your email below and we'll get back to you!</p>
                 
@@ -8,6 +8,8 @@ function Contact () {
                     <input type="text" placeholder="email@example.co" className="px-4 py-2 rounded-md text-xl font-sub font-medium text-black placeholder:text-gray-400" />
                     <button className="uppercase text-xl text-white bg-[#1c1c1c] px-8 py-2 rounded-md font-medium ease-in-out duration-200 hover:bg-white hover:text-black focus:text-black focus:bg-white"> send </button>
                 </div>
+
+                <div className="text-gray-200 w-full flex justify-end items-end mt-8"> copyrights@omar-risi</div>
         </section>
     )
 }
